1e4337c8c3e85354441e5354f15d79f93e6fa2c5
MyslivchenkoRI <MyslivchenkoRI@aeropribor.ru>
2024-07-31 15:03:06 +0300

    Update

--- MyQShellExecute.h
+++ MyQShellExecute.h
@@ -2,12 +2,23 @@
 #ifndef MyQShellExecute_H
 #define MyQShellExecute_H
 //---------------------------------------------------------------------------
-void ShellExecutePath(QString file)
-{
-	QFileInfo fileI(file);
-	QString command = QString("/select, \"") + fileI.fileName() + "\"";
-
-	ShellExecute(NULL, L"open", L"explorer.exe", command.toStdWString().c_str(), fileI.path().toStdWString().c_str(), SW_NORMAL);
-}
+#include <windows.h>
+#include <QString>
+#include <QFileInfo>
 //---------------------------------------------------------------------------
-#endif
+struct MyQShellExecute
+{
+    inline static void ShellExecuteFile(QString file, QString params = "", int SW_MODE = SW_NORMAL)
+    {
+	ShellExecute(NULL, NULL, file.toStdWString().c_str(), params.toStdWString().c_str(), NULL, SW_MODE);
+    }
+    inline static void ShellExecutePath(QString file, int SW_MODE = SW_NORMAL)
+    {
+	QFileInfo fileInfo(file);
+	QString command = QString("/select, \"") + fileInfo.fileName() + "\"";
+
+	ShellExecute(NULL, L"open", L"explorer.exe", command.toStdWString().c_str(), fileInfo.path().toStdWString().c_str(), SW_MODE);
+    }
+};
+//---------------------------------------------------------------------------
+#endif  // MyQShellExecute
