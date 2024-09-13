cad02ac2472bed9e53fa4d459e9b908a9cdca1e0
MyslivchenkoRI <MyslivchenkoRI@aeropribor.ru>
2024-09-13 15:55:31 +0300

    add float_to_bytes_conversion

--- MyCppDifferent.h
+++ MyCppDifferent.h
@@ -16,7 +16,6 @@ public:
     inline static std::string GetPathToExe();	// если программа запускается из среды Qt creator, то выдаёт путь не до конца
 						// пропускает последнюю папку debug или release
 						// если программа запущена уже как выпущенный exe-файл, то всё норм
-
     template<class T>
     struct reverted_container
     {
@@ -31,8 +30,14 @@ public:
     };
 
     template<class T>
-    static auto reverse(T &aContainer) { return reverted_container(aContainer); }
+    inline static auto reverse(T &aContainer) { return reverted_container(aContainer); }
+
+    inline static std::array<unsigned char,4> to_bytes(float value); // return[0] - нулевой байт value
+    inline static float to_float(std::array<unsigned char,4> bytes); // нулевой байт в return = bytes[0]
+    inline static bool test_float_to_bytes_conversion();
 };
+
+
 //---------------------------------------------------------------------------
 std::string MyCppDifferent::ToDiapasons(std::vector<int> vect)
 {
@@ -84,6 +89,72 @@ std::string MyCppDifferent::GetPathToExe()
 	std::cerr << "GetPathToExe ERROR getcwd returned NULL" << std::endl;
 	return "ERROR";
     }
+}
+
+std::array<unsigned char, 4> MyCppDifferent::to_bytes(float value)
+{
+    const int size = 4;
+    const int sizeOf = sizeof(value);
+    if(sizeOf != size) { std::cerr << "ToBytes error, sizeof(float) = " + std::to_string(sizeOf); return {}; }
+
+    std::array<unsigned char,size> ret;
+    unsigned char* ptr = reinterpret_cast<unsigned char*>(&value);
+    for(int i=0; i<size; i++)
+    {
+	ret[i] = ptr[i];
+    }
+
+    return ret;
+}
+
+float MyCppDifferent::to_float(std::array<unsigned char, 4> bytes)
+{
+    const int size = bytes.size();
+    const int sizeOf = sizeof(float);
+    if(sizeOf != size) { std::cerr << "ToBytes error, sizeof(float) = " + std::to_string(sizeOf); return {}; }
+
+    float ret;
+    unsigned char* ptr = reinterpret_cast<unsigned char*>(&ret);
+    for(int i=0; i<size; i++)
+    {
+	ptr[i] = bytes[i];
+    }
+
+    return ret;
+}
+
+bool MyCppDifferent::test_float_to_bytes_conversion()
+{
+    bool res = true;
+    srand(time(nullptr));
+
+    float from;
+    std::array<unsigned char,4> bytes;
+    float to;
+    for(int i=0; i<10; i++)
+    {
+	from = (float) rand() / rand();
+	bytes = to_bytes(from);
+	to = to_float(bytes);
+	if(from != to) { std::cerr << "bad conversion " << from << " -> " << to; res = false; }
+
+	from = (float) rand() / - rand();
+	bytes = to_bytes(from);
+	to = to_float(bytes);
+	if(from != to) { std::cerr << "bad conversion " << from << " -> " << to; res = false; }
+
+	from = (float) rand() / rand() * 999999.0f;
+	bytes = to_bytes(from);
+	to = to_float(bytes);
+	if(from != to) { std::cerr << "bad conversion " << from << " -> " << to; res = false; }
+
+	from = (float) rand() / - rand() * 999999.0f;
+	bytes = to_bytes(from);
+	to = to_float(bytes);
+	if(from != to) { std::cerr << "bad conversion " << from << " -> " << to; res = false; }
+    }
+
+    return res;
 }
 
 //---------------------------------------------------------------------------------------------------
