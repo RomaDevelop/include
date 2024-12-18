973035d31ad44c5ca44fd8e61b8b1f7142d8ca40
MyslivchenkoRI <MyslivchenkoRI@aeropribor.ru>
2024-12-18 19:13:04 +0300

    Update

--- MyQString.h
+++ MyQString.h
@@ -6,14 +6,10 @@
 
 struct MyQString
 {
-    inline static QStringList QStringListSized(int size, const QString &value = "")
-    {
-	QStringList ret;
-	ret.reserve(size);
-	for(int i=0; i<size; i++)
-	    ret.append(value);
-	return ret;
-    }
+    inline static QStringList QStringListSized(int size, const QString &value = "");
+
+    inline static QString& RightJistifie(QString &str_to_justifie, int width, QChar fill = QLatin1Char(' '), bool trunc = false);
+    inline static QString& LeftJistifie(QString &str_to_justifie, int width, QChar fill = QLatin1Char(' '), bool trunc = false);
 
     template<class T>
     inline static QString AsDebug(const T& obj)
@@ -25,5 +21,45 @@ struct MyQString
 	return ret;
     }
 };
+
+QStringList MyQString::QStringListSized(int size, const QString & value)
+{
+    QStringList ret;
+    ret.reserve(size);
+    for(int i=0; i<size; i++)
+	ret.append(value);
+    return ret;
+}
+
+QString & MyQString::RightJistifie(QString & str_to_justifie, int width, QChar fill, bool trunc)
+{
+    int size = str_to_justifie.size();
+    if(size > width)
+    {
+	if(trunc) return str_to_justifie.remove(0, size - width);
+	else return str_to_justifie;
+    }
+    if(size == width) return str_to_justifie;
+    str_to_justifie.resize(width);
+    for(int i=size-1, j=width-1; i>=0; i--, j--)
+	str_to_justifie[j]=str_to_justifie[i];
+    int inserting = width - size;
+    for(int i=0; i<inserting; i++)
+	str_to_justifie[i] = fill;
+    return str_to_justifie;
+}
+
+QString & MyQString::LeftJistifie(QString & str_to_justifie, int width, QChar fill, bool trunc)
+{
+    int size = str_to_justifie.size();
+    if(size > width)
+    {
+	if(trunc) { str_to_justifie.chop(size - width); return str_to_justifie; }
+	else return str_to_justifie;
+    }
+    if(size == width) return str_to_justifie;
+    str_to_justifie.resize(width, fill);
+    return str_to_justifie;
+}
 
 #endif
