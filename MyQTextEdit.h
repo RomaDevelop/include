0e7fd8c9d9875e36c651d8f6500378eab7166e4d
Мысливченко Роман Ильич <MyslivchenkoRI@aeropribor.ru>
2025-02-07 17:55:26 +0300

    Update

--- MyQTextEdit.h
+++ MyQTextEdit.h
@@ -6,40 +6,52 @@
 
 struct MyQTextEdit
 {
-    inline static void ColorizeLastRow(QTextEdit *textEdit, const QBrush &brush)
-    {
-	QTextCharFormat format;
-	format.setForeground(brush);
-
-	auto curs = textEdit->textCursor();
-	curs.movePosition(QTextCursor::End, QTextCursor::MoveAnchor);
-	curs.movePosition(QTextCursor::StartOfLine, QTextCursor::KeepAnchor);
-	curs.setCharFormat(format);
-	textEdit->repaint();
-    }
-
-    inline static void ColorizeLastCount(QTextEdit *textEdit, const QBrush &brush, uint count)
-    {
-	QTextCharFormat format;
-	format.setForeground(brush);
-
-	auto curs = textEdit->textCursor();
-	curs.setPosition(textEdit->document()->characterCount() - 1 - count, QTextCursor::MoveAnchor);
-	curs.movePosition(QTextCursor::End, QTextCursor::KeepAnchor);
-	curs.setCharFormat(format);
-	textEdit->repaint();
-    }
-
-    inline void Colorize(QTextEdit *textEdit, int from, int to, const QColor &color)
-    {
-	QTextCharFormat format;
-	format.setForeground(color);
-
-	auto cursor = textEdit->textCursor();
-	cursor.setPosition(from,QTextCursor::MoveAnchor);
-	cursor.setPosition(to,QTextCursor::KeepAnchor);
-	cursor.setCharFormat(format);
-    }
+    inline static void AppendInLastRow(QTextEdit *textEdit, const QString& text);
+    inline static void ColorizeLastRow(QTextEdit *textEdit, const QBrush &brush);
+    inline static void ColorizeLastCount(QTextEdit *textEdit, const QBrush &brush, uint count);
+    inline void Colorize(QTextEdit *textEdit, int from, int to, const QColor &color);
 };
+
+void MyQTextEdit::AppendInLastRow(QTextEdit * textEdit, const QString & text)
+{
+    auto cursor = textEdit->textCursor();
+    cursor.movePosition(QTextCursor::End, QTextCursor::MoveAnchor);
+    cursor.insertText(text);
+}
+
+void MyQTextEdit::ColorizeLastRow(QTextEdit * textEdit, const QBrush & brush)
+{
+    QTextCharFormat format;
+    format.setForeground(brush);
+
+    auto curs = textEdit->textCursor();
+    curs.movePosition(QTextCursor::End, QTextCursor::MoveAnchor);
+    curs.movePosition(QTextCursor::StartOfLine, QTextCursor::KeepAnchor);
+    curs.setCharFormat(format);
+    textEdit->repaint();
+}
+
+void MyQTextEdit::ColorizeLastCount(QTextEdit * textEdit, const QBrush & brush, uint count)
+{
+    QTextCharFormat format;
+    format.setForeground(brush);
+
+    auto curs = textEdit->textCursor();
+    curs.setPosition(textEdit->document()->characterCount() - 1 - count, QTextCursor::MoveAnchor);
+    curs.movePosition(QTextCursor::End, QTextCursor::KeepAnchor);
+    curs.setCharFormat(format);
+    textEdit->repaint();
+}
+
+void MyQTextEdit::Colorize(QTextEdit * textEdit, int from, int to, const QColor & color)
+{
+    QTextCharFormat format;
+    format.setForeground(color);
+
+    auto cursor = textEdit->textCursor();
+    cursor.setPosition(from,QTextCursor::MoveAnchor);
+    cursor.setPosition(to,QTextCursor::KeepAnchor);
+    cursor.setCharFormat(format);
+}
 
 #endif
