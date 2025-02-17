b75d6404a9b18036041fefefb0481032b0e5fb8f
Roma_home <26rus_mri@mail.ru>
2025-02-17 19:59:50 +0300

    Update MyQTableView

--- MyQTableView.h
+++ MyQTableView.h
@@ -4,16 +4,23 @@
 #include <QDebug>
 #include <QTableView>
 #include <QMessageBox>
+#include <QStyledItemDelegate>
+#include <QPainter>
 
 #include "MyQShortings.h"
+#include "MyQString.h"
+
+class ColoriserDelegate;
 
 class MyQTableView : public QTableView
 {
 	Q_OBJECT
 public:
-	explicit MyQTableView(QWidget *parent = nullptr) : QTableView{parent} {}
+	inline explicit MyQTableView(QWidget *parent = nullptr);
 
-	QString saveColumnWidths()
+	ColoriserDelegate *coloriserDelegate;
+
+	inline QString saveColumnWidths()
 	{
 		QString ret;
 		for (int column = 0; column < model()->columnCount(); ++column) {
@@ -21,7 +28,7 @@ public:
 		}
 		return ret;
 	}
-	void restoreColumnWidths(const QString &widthsStr)
+	inline void restoreColumnWidths(const QString &widthsStr)
 	{
 		QStringList widths = widthsStr.split(";", QString::SkipEmptyParts);
 		for(int i=0; i<widths.size() && i<model()->columnCount(); i++)
@@ -33,16 +40,99 @@ public:
 		}
 	}
 
-	auto currentRow() { return currentIndex().row(); }
-	auto cellData(int row, int col) { return model()->index(row, col).data(); }
-	auto cellDataStr(int row, int col) { return cellData(row, col).toString(); }
+	inline int RowsCount(bool do_fetch);
 
-	auto currentRecordData(int col) { return model()->index(currentIndex().row(), col).data(); }
-	auto currentRecordDataStr(int col) { return currentRecordData(col).toString(); }
+	inline auto currentRow() { return currentIndex().row(); }
+	inline auto cellData(int row, int col) { return model()->index(row, col).data(); }
+	inline auto cellDataStr(int row, int col) { return cellData(row, col).toString(); }
 
-	bool Locate(const QString &fieldName, const QString &fieldValue);
-	bool LocateRow(int row, int col);
+	inline auto currentRecordData(int col) { return model()->index(currentIndex().row(), col).data(); }
+	inline auto currentRecordDataStr(int col) { return currentRecordData(col).toString(); }
+
+	inline bool Locate(const QString &fieldName, const QString &fieldValue);
+	inline bool LocateRow(int row, int col);
+
+	///\brief for empty feildsIndexes return all fields
+	inline std::vector<QStringList> ToTable(std::vector<int> feildsIndexes)
+	{
+		QAbstractItemModel *model = this->model();
+		if (!model) return {};
+		std::vector<QStringList> table;
+		int colCount = model->columnCount();
+		if(feildsIndexes.empty())
+		{
+			for (int row = 0; row < model->rowCount(); ++row)
+			{
+				auto &retRow = table.emplace_back();
+				for(int column=0; column<colCount; column++)
+				{
+
+					retRow += model->index(row, column).data().toString();
+				}
+
+				// догрузка записей
+				if(row >= model->rowCount()-2 && model->canFetchMore(QModelIndex()))
+					model->fetchMore(QModelIndex());
+			}
+		}
+		else
+		{
+			auto removeRes = std::remove_if(feildsIndexes.begin(),feildsIndexes.end(),[colCount](int n){ return n>=colCount || n<0; });
+			if(removeRes != feildsIndexes.end()) QMbError("Wring indexes in feildsIndexes ["+MyQString::AsDebug(feildsIndexes)+"]");
+			feildsIndexes.erase(removeRes, feildsIndexes.end());
+			int size = feildsIndexes.size();
+			for (int row = 0; row < model->rowCount(); ++row)
+			{
+				auto &retRow = table.emplace_back();
+				for(int i=0; i<size; i++)
+				{
+					retRow += model->index(row, feildsIndexes[i]).data().toString();
+				}
+
+				// догрузка записей
+				if(row >= model->rowCount()-2 && model->canFetchMore(QModelIndex()))
+					model->fetchMore(QModelIndex());
+			}
+		}
+		return table;
+	}
 };
+
+class ColoriserDelegate : public QStyledItemDelegate {
+public:
+	ColoriserDelegate(QObject *parent = nullptr)
+		: QStyledItemDelegate(parent) {}
+
+	void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const override {
+		// Проверяем, является ли текущая строка выделенной
+		if (highliteRule && highliteRule(index)) {
+			painter->fillRect(option.rect, color); // Цвет выделения
+		}
+		// Вызываем базовый метод для отрисовки ячейки
+		QStyledItemDelegate::paint(painter, option, index);
+	}
+
+	std::function<bool(const QModelIndex &currentIndex)> highliteRule;
+	QColor color;
+};
+
+MyQTableView::MyQTableView(QWidget *parent) : QTableView{parent}
+{
+	coloriserDelegate = new ColoriserDelegate(this);
+	setItemDelegate(coloriserDelegate);
+}
+
+int MyQTableView::RowsCount(bool do_fetch)
+{
+	QAbstractItemModel *model = this->model();
+	if(do_fetch)
+	{
+		while (model->canFetchMore(QModelIndex())) {
+			model->fetchMore(QModelIndex());
+		}
+	}
+	return model->rowCount();
+}
 
 inline bool MyQTableView::Locate(const QString &fieldName, const QString &fieldValue)
 {
@@ -63,7 +153,7 @@ inline bool MyQTableView::Locate(const QString &fieldName, const QString &fieldV
 	for (int row = 0; row < model->rowCount(); ++row)
 	{
 		QModelIndex index = model->index(row, column);
-		if (index.isValid() && model->data(index).toString() == fieldValue)
+		if(index.data().toString() == fieldValue)
 		{
 			this->setCurrentIndex(index);
 			this->scrollTo(index, QAbstractItemView::PositionAtTop);
