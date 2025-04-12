21d820307d09160e9780d0b427dd276bd71ffaa3
MyslivchenkoRI <MyslivchenkoRI@aeropribor.ru>
2025-04-12 17:19:16 +0300

    Update

--- declare_struct.h
+++ declare_struct.h
@@ -5,16 +5,16 @@
     struct struct_name {            \
         type1 name1;                \
         type2 name2;                \
-		explicit struct_name(): name1{}, name2{} {}    \
-		explicit struct_name(type1 name1, type2 name2): name1{std::move(name1)}, name2{std::move(name2)} {}  \
+		struct_name(): name1{}, name2{} {}    \
+		struct_name(type1 name1, type2 name2): name1{std::move(name1)}, name2{std::move(name2)} {}  \
     }
 
 #define declare_struct_2_fields_no_move(struct_name, type1, name1, type2, name2)    \
     struct struct_name {            \
         type1 name1;                \
         type2 name2;                \
-		explicit struct_name(): name1{}, name2{} {}    \
-		explicit struct_name(type1 name1, type2 name2): name1{name1}, name2{name2} {}    \
+		struct_name(): name1{}, name2{} {}    \
+		struct_name(type1 name1, type2 name2): name1{name1}, name2{name2} {}    \
     }
 
 #define declare_struct_3_fields_move(struct_name, type1, name1, type2, name2, type3, name3) \
@@ -22,8 +22,8 @@
         type1 name1;                \
         type2 name2;                \
         type3 name3;                \
-		explicit struct_name(): name1{}, name2{}, name3{} {}    \
-		explicit struct_name(type1 name1, type2 name2, type3 name3): name1{std::move(name1)}, name2{std::move(name2)}, name3{std::move(name3)} {}    \
+		struct_name(): name1{}, name2{}, name3{} {}    \
+		struct_name(type1 name1, type2 name2, type3 name3): name1{std::move(name1)}, name2{std::move(name2)}, name3{std::move(name3)} {}    \
     }
 
 #define declare_struct_3_fields_no_move(struct_name, type1, name1, type2, name2, type3, name3)  \
@@ -31,8 +31,8 @@
         type1 name1;                \
         type2 name2;                \
         type3 name3;                \
-		explicit struct_name(): name1{}, name2{}, name3{} {}    \
-		explicit struct_name(type1 name1, type2 name2, type3 name3): name1{name1}, name2{name2}, name3{name3} {}	\
+		struct_name(): name1{}, name2{}, name3{} {}    \
+		struct_name(type1 name1, type2 name2, type3 name3): name1{name1}, name2{name2}, name3{name3} {}	\
     }
 
 #define declare_struct_4_fields_move(struct_name, type1, name1, type2, name2, type3, name3, type4, name4)   \
@@ -41,8 +41,8 @@
         type2 name2;                \
         type3 name3;                \
         type4 name4;                \
-		explicit struct_name(): name1{}, name2{}, name3{}, name4{} {}    \
-		explicit struct_name(type1 name1, type2 name2, type3 name3, type4 name4): name1{std::move(name1)}, name2{std::move(name2)}, name3{std::move(name3)}, name4{std::move(name4)} {} \
+		struct_name(): name1{}, name2{}, name3{}, name4{} {}    \
+		struct_name(type1 name1, type2 name2, type3 name3, type4 name4): name1{std::move(name1)}, name2{std::move(name2)}, name3{std::move(name3)}, name4{std::move(name4)} {} \
     }
 
 #define declare_struct_4_fields_no_move(struct_name, type1, name1, type2, name2, type3, name3, type4, name4)    \
@@ -51,8 +51,8 @@
         type2 name2;                \
         type3 name3;                \
         type4 name4;                \
-		explicit struct_name(): name1{}, name2{}, name3{}, name4{} {}     \
-		explicit struct_name(type1 name1, type2 name2, type3 name3, type4 name4): name1{name1}, name2{name2}, name3{name3}, name4{name4} {}  \
+		struct_name(): name1{}, name2{}, name3{}, name4{} {}     \
+		struct_name(type1 name1, type2 name2, type3 name3, type4 name4): name1{name1}, name2{name2}, name3{name3}, name4{name4} {}  \
     }
 
 #endif
