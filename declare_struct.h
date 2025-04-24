a506397b01a8fc293fdd2b456b0b5fe730831107
MyslivchenkoRI <MyslivchenkoRI@aeropribor.ru>
2025-04-24 18:06:31 +0300

    declare_struct_5_fields_move added, but commented

--- declare_struct.h
+++ declare_struct.h
@@ -55,4 +55,30 @@
 		struct_name(type1 name1, type2 name2, type3 name3, type4 name4): name1{name1}, name2{name2}, name3{name3}, name4{name4} {}  \
     }
 
+/*
+#define declare_struct_5_fields_move(struct_name, type1, name1, type2, name2, type3, name3, type4, name4, type5, name5)   \
+	struct struct_name {            \
+		type1 name1;                \
+		type2 name2;                \
+		type3 name3;                \
+		type4 name4;                \
+		type5 name5;				\
+		struct_name(): name1{}, name2{}, name3{}, name4{}, name5{} {}    \
+		struct_name(type1 name1, type2 name2, type3 name3, type4 name4, type5 name5): name1{std::move(name1)}, name2{std::move(name2)}, \
+			name3{std::move(name3)}, name4{std::move(name4)}, name5{std::move(name5)} {} \
+	}
+
+//#define declare_struct_5_fields_no_move(struct_name, type1, name1, type2, name2, type3, name3, type4, name4, type5, name5)    \
+	struct struct_name {            \
+		type1 name1;                \
+		type2 name2;                \
+		type3 name3;                \
+		type4 name4;                \
+		type5 name5;				\
+		struct_name(): name1{}, name2{}, name3{}, name4{}, name5{} {}    \
+		struct_name(type1 name1, type2 name2, type3 name3, type4 name4, type5 name5): name1{name1}, name2{name2}, name3{name3}, name4{name4}, \
+			name5{name5} {}  \
+	}
+*/
+
 #endif
