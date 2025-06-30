284872785169bbb341be4519ecbf1443a54dbd19
MyslivchenkoRI <MyslivchenkoRI@aeropribor.ru>
2025-06-30 18:11:15 +0300

    MyQWidget: add AdjustWidgetPosition

--- MyQWidget.h
+++ MyQWidget.h
@@ -1,6 +1,8 @@
 #ifndef MyQWidget_H
 #define MyQWidget_H
 
+#include <QApplication>
+#include <QScreen>
 #include <QDebug>
 #include <QStringList>
 #include <QFontMetrics>
@@ -20,6 +22,8 @@ struct MyQWidget
 	inline static void SetFontBold(QAction *obj, bool enable);
 	/* Можно сделать шаблонную функцию, но тогда копилятор будет создавать функции для каждого наследника QWidget, а их очень много
 	 * Когда перейдем на С++ 20, попробовать через концепты этого избежать */
+
+	inline static void AdjustWidgetPosition(QWidget *widget, int edgeDistance = 0);
 };
 
 //------------------------------------------------------------------------------------------------------------------------------------------
@@ -58,6 +62,35 @@ void MyQWidget::SetFontBold(QAction *obj, bool enable)
 	auto font = obj->font();
 	font.setBold(enable);
 	obj->setFont(font);
+}
+
+void MyQWidget::AdjustWidgetPosition(QWidget *widget, int edgeDistance) {
+	// Получаем текущую позицию виджета
+	QPoint pos = widget->pos();
+
+	// Получаем экран, на котором находится виджет
+	QScreen *screen = QApplication::screenAt(pos);
+	if(!screen) screen = QApplication::primaryScreen();
+
+	if(screen) {
+		QRect screenGeometry = screen->geometry();
+
+		// Проверяем, выходит ли виджет за границы экрана
+		if(pos.x() < screenGeometry.left()+ edgeDistance) {
+			pos.setX(screenGeometry.left() + edgeDistance);
+		} else if (pos.x() + widget->width() > screenGeometry.right() - edgeDistance) {
+			pos.setX(screenGeometry.right() - widget->width() - edgeDistance);
+		}
+
+		if(pos.y() < screenGeometry.top() + edgeDistance) {
+			pos.setY(screenGeometry.top() + edgeDistance);
+		} else if (pos.y() + widget->height() > screenGeometry.bottom() - edgeDistance) {
+			pos.setY(screenGeometry.bottom() - widget->height() - edgeDistance);
+		}
+
+		// Устанавливаем новую позицию виджета
+		widget->move(pos);
+	}
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------
