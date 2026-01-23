f4dd5ca3411f0a25a0610db366f05a10d96d2a01
MyslivchenkoRI <MyslivchenkoRI@aeropribor.ru>
2026-01-23 13:43:40 +0300

    MyQTabWidget: add moving window into tabs

--- MyQTabWidget.h
+++ MyQTabWidget.h
@@ -1,13 +1,14 @@
 //----------------------------------------------------------------------------------------------------------------
 #ifndef MyQTabWidget_H
 #define MyQTabWidget_H
-//----------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------------
 #include <functional>
 
+#include <QDebug>
 #include <QMouseEvent>
 #include <QTabBar>
 #include <QTabWidget>
-//----------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------------
 
 class DetachFilter;
 
@@ -16,7 +17,7 @@ class MyQTabWidget : public QTabWidget
 	Q_OBJECT
 
 public:
-	explicit MyQTabWidget(QWidget *parent = nullptr) : QTabWidget(parent) {}
+	explicit MyQTabWidget(QWidget *parent = nullptr) : QTabWidget(parent) { setAcceptDrops(true); }
 	virtual ~MyQTabWidget() = default;
 
 	///\brief Enable mechanics tabs detaching when dragging them vertically
@@ -24,11 +25,35 @@ public:
 	/// can be called repeatedly to reset detachDistance
 	inline virtual void EnableDetaching(int detachDistance = 50);
 
+	inline bool IsOverTabs(const QPoint &globalPos);
+	inline void UpdatePlaceholder(const QPoint &globalPos, const QString &text);
+	inline void ReplacePlaceholder(QString caption, QWidget *window);
+
 private:
 	DetachFilter *detachFilter {};
+
+	QWidget *tabPlaceholder = {};
 };
 
-//----------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------------
+
+class DetachableWindow : public QWidget {
+	Q_OBJECT
+public:
+	inline explicit DetachableWindow(MyQTabWidget *tabWidget, std::function<QString()> GetTabCaption, QWidget *parent = nullptr);
+	~DetachableWindow() = default;
+
+protected:
+	inline bool event(QEvent *event) override;
+	inline void mouseReleaseEvent(QMouseEvent *event) override;
+
+private:
+	MyQTabWidget *m_targetTabWidget = nullptr;
+	std::function<QString()> GetTabCaption;
+	bool m_isDragging = false;
+};
+
+//--------------------------------------------------------------------------------------------------------------------------
 
 class DetachFilter : public QObject
 {
@@ -88,7 +113,54 @@ void MyQTabWidget::EnableDetaching(int detachDistance)
 	}
 }
 
-//----------------------------------------------------------------------------------------------------------------
+bool MyQTabWidget::IsOverTabs(const QPoint &globalPos)
+{
+	QPoint localPos = tabBar()->mapFromGlobal(globalPos);
+	return tabBar()->rect().contains(localPos);
+}
+
+void MyQTabWidget::UpdatePlaceholder(const QPoint &globalPos, const QString &text) {
+	// Если мышь НЕ над таб-баром — удаляем заглушку и выходим
+	if(!IsOverTabs(globalPos)) {
+		if (tabPlaceholder) {
+			delete tabPlaceholder;
+			tabPlaceholder = nullptr;
+		}
+		return;
+	}
+
+	QPoint localPos = tabBar()->mapFromGlobal(globalPos);
+
+	// Если мышь над таб-баром, а заглушки еще нет — создаем
+	if (!tabPlaceholder) {
+		tabPlaceholder = new QWidget();
+		int index = tabBar()->tabAt(localPos);
+		if (index == -1) index = count();
+		insertTab(index, tabPlaceholder, text);
+		setCurrentWidget(tabPlaceholder);
+	} else {
+		// Если заглушка уже есть, просто двигаем её под курсор
+		int currentIndex = indexOf(tabPlaceholder);
+		int targetIndex = tabBar()->tabAt(localPos);
+		if (targetIndex != -1 && targetIndex != currentIndex) {
+			tabBar()->moveTab(currentIndex, targetIndex);
+		}
+	}
+}
+
+void MyQTabWidget::ReplacePlaceholder(QString caption, QWidget *window)
+{
+	int index = 0;
+	if(tabPlaceholder) index = indexOf(tabPlaceholder);
+	else qDebug() << "ReplacePlaceholder called for invalid placeholder";
+	if(index < 0) { qDebug() << "ReplacePlaceholder, but m_placeholder has invalid index"; index = 0; }
+	insertTab(index, window, caption);
+	setCurrentIndex(index);
+	delete tabPlaceholder;
+	tabPlaceholder = nullptr;
+}
+
+//--------------------------------------------------------------------------------------------------------------------------
 
 bool DetachFilter::eventFilter(QObject *obj, QEvent *event)
 {
@@ -135,6 +207,57 @@ bool DetachFilter::eventFilter(QObject *obj, QEvent *event)
 	return false;
 }
 
-//----------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------------
+
+DetachableWindow::DetachableWindow(MyQTabWidget *tabWidget, std::function<QString ()> GetTabCaption, QWidget *parent) :
+	QWidget(parent) ,
+	m_targetTabWidget { tabWidget },
+	GetTabCaption { GetTabCaption }
+{}
+
+bool DetachableWindow::event(QEvent *event) {
+	// Обработка нажатия кнопки на заголовке
+	if (event->type() == QEvent::NonClientAreaMouseButtonPress) {
+		QMouseEvent *mouseEvent = static_cast<QMouseEvent*>(event);
+		if (mouseEvent->button() == Qt::LeftButton) {
+			// Тут нужна ещё проверка, что клик именно в заголовок, а не в иной NonClientAreaMouseButtonPress
+			m_isDragging = true;
+		}
+	}
+
+	if (event->type() == QEvent::Move) {
+		// обрабатываем именно Move, потому что когда мы держим заголовок,
+		// NonClientAreaMouseButtonMove не вызывается (возможно потому что мышь не двигается относительно окна)
+		if (m_targetTabWidget and m_isDragging)
+		{
+			m_targetTabWidget->UpdatePlaceholder(QCursor::pos(), GetTabCaption());
+
+			if(m_targetTabWidget->IsOverTabs(QCursor::pos())) setWindowOpacity(0.3); // Окно станет прозрачным
+			else setWindowOpacity(1);
+		}
+	}
+
+	// Обработка отпускания кнопки на заголовке
+	if (event->type() == QEvent::NonClientAreaMouseButtonRelease) {
+		if(m_isDragging)
+		{
+			if(m_targetTabWidget->IsOverTabs(QCursor::pos()))
+			{
+				setWindowOpacity(1);
+				m_targetTabWidget->ReplacePlaceholder(GetTabCaption(), this);
+			}
+		}
+		m_isDragging = false;
+	}
+
+	return QWidget::event(event);
+}
+
+void DetachableWindow::mouseReleaseEvent(QMouseEvent *event) {
+	m_isDragging = false;
+	QWidget::mouseReleaseEvent(event);
+}
+
+//--------------------------------------------------------------------------------------------------------------------------
 #endif
-//----------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------------
